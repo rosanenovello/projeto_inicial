ca689656369433a8c4563431b129489dc0524a7d
Rosane Novello <rosanenovello@gmail.com>
2024-06-24 12:20:11 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Jornada de Dados")
+
+#variaveis
+idade=51
+nome="Rosane"
+
+# Print
+print(nome)
+print(idade)
+
+# Calculos
+
+nova_idade = idade + 1
+print(nova_idade)
+
+# Listas
+lista = [1,2,3,4,5]
+
+# Metodos
+lista_invertida = lista.reverse()
